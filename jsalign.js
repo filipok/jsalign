7a94f02ab352ab5e14122a387b4bf15b4265d477
Filip <filip.gadiuta@gmail.com>
2015-12-07 01:35:21 +0200

    adding 'add' button and reorg code

--- jsalign.js
+++ jsalign.js
@@ -1,18 +1,27 @@
 //<![CDATA[
-<!-- using code from http://jsfiddle.net/X33px/2/-->
-$(window).load(function(){
-$('div').each(function() {
-  $(this).prepend('<a class="button delete" href="#">Delete</a>  <a href="#" class="button edit">Edit</a> <a class="button merge" href="#">Merge with next</a>')
+// using code from http://jsfiddle.net/X33px/2/
+
+$(document).on('click', 'a.add', function() {
+  var val = $(this).parent().next().html();
+  $(this).parent().next().replaceWith('<div class="cell">' +
+    '<a class="button add" href="#">Add new</a>' + 
+    '<a class="button delete" href="#">Delete</a>' + 
+    '<a href="#" class="button edit">Edit</a>' + 
+    '<a class="button merge" href="#">Merge with next</a>' + 
+    '<span class="celltext"></span></div>' +
+    '<div class="cell">' + val + '</div>');
+  return false;
 });
 
-$('div a.delete').on('click', function() {
+
+$(document).on('click', 'a.delete', function() {
   if (window.confirm("Are you sure you want to delete this segment?")) {
     $(this).parent().remove();
   };
   return false;
 });
 
-$('div a.merge').on('click', function() {
+$(document).on('click', 'a.merge', function() {
   if (window.confirm("Are you sure you want to merge this segment?")) {
     var val = $(this).parent().next().children('span').html();
     $(this).parent().children('span').append(" " + val);
@@ -21,8 +30,7 @@ $('div a.merge').on('click', function() {
   return false;
 });
 
-
-$('div a.edit').on('click', function() {
+$(document).on('click', 'a.edit', function() {
   var val = $(this).siblings('span').html();
   if (val === "") {
     val = "No text.";
@@ -41,4 +49,13 @@ $('div a.edit').on('click', function() {
   }
   return false;
 });
+
+$(window).load(function(){
+$('div').each(function() {
+  $(this).prepend('<a class="button add" href="#">Add new</a>' + 
+    '<a class="button delete" href="#">Delete</a>' +
+    ' <a href="#" class="button edit">Edit</a>' +
+    '<a class="button merge" href="#">Merge with next</a>');
+});
+
 });//]]> 
